e6af018bcb7a560ae6bd7342a095441b46156e44
Your Name <you@example.com>
2024-10-08 15:49:07 +0700

    26/09/2024 update fe and route

---
+++ website/ecommerce/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import *
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
--- website/ecommerce/views.py
+++ website/ecommerce/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import  api_view
 from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import *
 
 
 # Create your views here.
+class Account(APIView):
+    def get(self, request):
+        user = Users.objects.filter(email=request.user.email)
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
 def index(request):
     return render(request, 'home/index.html')
 
@@ -16,10 +30,8 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact/index.html')
-
 def login(request):
     return render(request, 'account/login.html')
-
 def register(request):
     return render(request, 'account/signup.html')
 
